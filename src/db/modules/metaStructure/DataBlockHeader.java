b673b1e022288aa04573b2926aba264c94d5feb3
mariohd <mariohenriquediniz@gmail.com>
2017-03-22 09:43:45 -0300

    add method load to data block header

--- src/db/modules/metaStructure/DataBlockHeader.java
+++ src/db/modules/metaStructure/DataBlockHeader.java
@@ -22,22 +22,40 @@ public class DataBlockHeader {
 		this.usedSpace = Utils.toByteArray(8, 3);
 		this.blockBeginning = FileSystem.pageSize * blockId;
 	}
-	
+
 	public boolean save() throws IOException {
 		RandomAccessFile file = parent.getContainerFile();
-		
+
 		file.seek(blockBeginning);
 		file.write(container);
-		
+
 		file.seek(blockBeginning + 1);
 		file.write(blockId);
-		
+
 		file.seek(blockBeginning + 4);
 		file.write(blockType);
-		
+
 		file.seek(blockBeginning + 5);
 		file.write(usedSpace);
-		
+
+		return true;
+	}
+
+	public boolean load() throws IOException {
+		RandomAccessFile file = parent.getContainerFile();
+
+		file.seek(blockBeginning);
+		this.container = file.readByte();
+
+		file.seek(blockBeginning + 1);
+		file.read(this.blockId);
+
+		file.seek(blockBeginning + 4);
+		this.blockType = file.readByte();
+
+		file.seek(blockBeginning + 5);
+		file.read(this.usedSpace);
+
 		return true;
 	}
 	
@@ -45,4 +63,11 @@ public class DataBlockHeader {
 		return this.blockBeginning + Utils.toInt(usedSpace, 0);
 	}
 	
+	public void print() {
+		System.out.println("\tCONTAINER: " + this.container);
+		System.out.println("\tID DO BLOCO: " + Utils.toInt(blockId, 0));
+		System.out.println("\tTIPO DO BLOCK: " + this.blockType);
+		System.out.println("\tESPAÇO USADO: " + Utils.toInt(this.usedSpace, 0));
+	}
+
 }
